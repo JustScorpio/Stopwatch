817f14a303b9fee6e6cc3100151fb826139d1ba0
Kosarev <skorpiello@gmail.com>
2022-04-23 14:43:08 +0500

    Добавьте файлы проекта.

---
+++ Stopwatch/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Stopwatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelCurTime = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timerCurTime = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 73);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(60, 30);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(78, 73);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(60, 30);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Location = new System.Drawing.Point(144, 73);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(60, 30);
+            this.buttonRestart.TabIndex = 2;
+            this.buttonRestart.Text = "Restart";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Current time:";
+            // 
+            // labelCurTime
+            // 
+            this.labelCurTime.AutoSize = true;
+            this.labelCurTime.Location = new System.Drawing.Point(169, 41);
+            this.labelCurTime.Name = "labelCurTime";
+            this.labelCurTime.Size = new System.Drawing.Size(35, 13);
+            this.labelCurTime.TabIndex = 5;
+            this.labelCurTime.Text = "label2";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // timerCurTime
+            // 
+            this.timerCurTime.Enabled = true;
+            this.timerCurTime.Interval = 10000;
+            this.timerCurTime.Tick += new System.EventHandler(this.timerCurTime_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 110);
+            this.Controls.Add(this.labelCurTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonRestart);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelCurTime;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer timerCurTime;
+    }
+}
+
---
+++ Stopwatch/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stopwatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            updateTextBox();
+            updateCurTimeLabel();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+            buttonStop.Enabled = true;
+            buttonStart.Enabled = false;
+            buttonRestart.Enabled = false;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            buttonStop.Enabled = false;
+            buttonStart.Enabled = true;
+            buttonRestart.Enabled = true;
+        }
+
+        private void buttonRestart_Click(object sender, EventArgs e)
+        {
+            milisecCounter = 0;
+            secCounter = 0;
+            minCounter = 0;
+            hourCounter = 0;
+
+            updateTextBox();
+
+            buttonStop.Enabled = false;
+            buttonStart.Enabled = true;
+            buttonRestart.Enabled = false;
+        }
+
+        int milisecCounter;
+        int secCounter;
+        int minCounter;
+        int hourCounter;
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            milisecCounter++;
+            if (milisecCounter == 100)
+            {
+                milisecCounter = 0;
+                secCounter++;
+                if (secCounter == 60)
+                {
+                    secCounter = 0;
+                    minCounter++;
+                    if (minCounter == 60)
+                    {
+                        minCounter = 0;
+                        hourCounter++;
+                    }
+                }
+            }
+
+            updateTextBox();
+        }
+
+        private void updateTextBox()
+        {
+            textBox1.Text = string.Format("{0}:{1}:{2}:{3}", hourCounter, minCounter, secCounter, milisecCounter);
+        }
+
+        private void updateCurTimeLabel()
+        {
+            labelCurTime.Text = DateTime.Now.ToString("t", System.Globalization.CultureInfo.GetCultureInfo("de-DE"));
+        }
+
+        private void timerCurTime_Tick(object sender, EventArgs e)
+        {
+            updateCurTimeLabel();
+        }
+    }
+}
